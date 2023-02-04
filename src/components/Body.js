ad5176272ae7c3ac01555dec042d983d82023df7
Aghil P Wilson <aghilpwilson1994@gmail.com>
2023-02-04 16:26:17 +0530

    implemented Search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,10 @@
-import React,{useState}from 'react'
+import React,{useEffect, useState}from 'react'
 import {restaurantData} from "./constants"
 import RestaurantCard from "./RestaurantCard"
+import ShimmerUi from './ShimmerUi';
 
-function filterData(searchText, restaurants) {
-    const filterData = restaurants.filter((restaurant) =>
+function filterData(searchText, allRestaurants) {
+    const filterData = allRestaurants.filter((restaurant) =>
     restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
   );
   return filterData;
@@ -11,8 +12,22 @@ function filterData(searchText, restaurants) {
   }
 function Body() {
     const [searchText, setSearchText] = useState("");
-    const [restaurants, setRestaurants] = useState(restaurantData);
-  return (
+    const [allRestaurants, setAllRestaurants] = useState([]);
+    const[filteredRestaurants,setFilteredRestaurants] = useState([])
+    useEffect(()=>{
+      console.log("useEffect Triggered")
+      getRestaurants()
+    },[])
+    const getRestaurants = async()=>{
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.14287788958732&lng=73.00082512199879&page_type=DESKTOP_WEB_LISTING");
+      const json = await data.json()
+      setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+      setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+      //console.log()
+     // console.log(filteredRestaurants)
+    }
+    if (!allRestaurants) return null;
+  return (allRestaurants.length===0)?<ShimmerUi/>: (
     <>
     <input 
     className = "searchbar"type="text" 
@@ -24,16 +39,17 @@ function Body() {
     <button 
     className="search-btn"
     onClick={() => {
-        console.log("cl")
+        //console.log("cl")
             // filter the data
-            const data = filterData(searchText, restaurants);
+            const data = filterData(searchText,allRestaurants);
             // update the state of restaurants list
-            setRestaurants(data);
-            console.log(restaurants)
+            setFilteredRestaurants(data)
+            // setAllRestaurants(data);
+            //console.log(restaurants)
           }}
         >Search</button>
     <div className="cards-container">
-       {restaurants.map((restaurant)=>{
+       {filteredRestaurants.map((restaurant)=>{
         return(
             <RestaurantCard key={restaurant.data.id} {...restaurant.data}/>
         )
---
+++ src/components/ShimmerUi.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShimmerUi() {
+  return (
+    <div>
+        <h1>Restaurants Are Loading.....</h1>
+    </div>
+  )
+}
+
+export default ShimmerUi
